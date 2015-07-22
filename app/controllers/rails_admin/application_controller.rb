e89fae7d1c1bb677b982cd72643fa9dc8b493c5f
Aravind <aravind@f22labs.com>
2015-07-22 21:17:46 +0530

    updated applicatipon controller

--- app/controllers/rails_admin/application_controller.rb
+++ app/controllers/rails_admin/application_controller.rb
@@ -29,7 +29,9 @@ module RailsAdmin
     end
 
     def get_object
-      fail(RailsAdmin::ObjectNotFound) unless (@object = @abstract_model.unscoped.get(params[:id]))
+      raise RailsAdmin::ObjectNotFound unless (object = @abstract_model.model.unscoped.find(params[:id]))
+      @object = RailsAdmin::Adapters::ActiveRecord::AbstractObject.new(object)
+      # fail(RailsAdmin::ObjectNotFound) unless (@object = @abstract_model.unscoped.get(params[:id]))
     end
 
     def to_model_name(param)
